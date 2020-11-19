d8eda0b62e3d79c0c39b8587e1b5f23a0c786e57
Victor Coelho <victor09120@gmail.com>
2020-11-19 16:26:26 -0300

    Added more tests for Exercise 1

--- cnpj_test.py
+++ cnpj_test.py
@@ -1,12 +1,23 @@
 import pytest
 from main import check_cnpj_string
 
+# Test for invalid characters where it should be a punctiation character.
 def test_invalid_punct():
     assert check_cnpj_string("02~123=683`0975/31") == "Invalid formatted CNPJ: incorrect punctuation."
     assert check_cnpj_string("02s123.683/0975-31") == "Invalid formatted CNPJ: incorrect punctuation."
+# Test for invalid CNPJ string size.
 def test_invalid_size():
     assert check_cnpj_string("021236830975") == "Invalid CNPJ format: invalid number of characters."
     assert check_cnpj_string("1111111111111111111") == "Invalid CNPJ format: invalid number of characters."
+    assert check_cnpj_string("")
+    assert check_cnpj_string("1")
+# Test for invalid characters - there should be only numbers or the correct punctuation characters.
 def test_invalid_characters():
     assert check_cnpj_string("02123683d9753a") == "Invalid formatted CNPJ: incorrect characters;  all characters must be numbers."
     assert check_cnpj_string("02.123.683/0975-3a") == "Invalid formatted CNPJ: incorrect characters;  all characters, aside from punctiation characters, must be numbers."
+# Tests for valid input of a formatted CNPJ string
+def test_valid_formatted_string():
+    assert check_cnpj_string("12.345.678/9012-34") == "CNPJ is correctly formatted."
+# Tests for valid input of a numbers only CNPJ string
+def test_valid_numbers_only_string():
+    assert check_cnpj_string("12345678901234") == "CNPJ is correctly formatted."
--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def check_cnpj_string(cnpj):
       if(len(unformatted_cnpj) != 14):
         return "Invalid formatted CNPJ: incorrect characters;  all characters, aside from punctiation characters, must be numbers."
       else:
-        return "CNPJ is correctly formatted"
+        return "CNPJ is correctly formatted."
     # For unformatted CNPJ entries, checks if all characters are numbers.
     for i in cnpj:
       if ord(i) not in range(48,58):
@@ -25,14 +25,5 @@ def check_cnpj_string(cnpj):
   # If cnpj entry does not have the correct length, it is not valid.
   else:
     return "Invalid CNPJ format: invalid number of characters."
-def main():
-  print(check_cnpj_string("02123683097531")) # correctly formatted
-  print(check_cnpj_string("02123683d9753a")) # invalid character
-  print(check_cnpj_string("021236830975")) # invalid size
-  print(check_cnpj_string("02.123.683/0975-31")) # correctly formatted
-  print(check_cnpj_string("02.123.683/0975-3a")) # invalid character
-  print(check_cnpj_string("02~123=683`0975/31")) # invalid punctiation
-
-
 if __name__ == "__main__":
     main()
